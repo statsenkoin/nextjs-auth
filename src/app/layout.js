02c7fc365177fd74ede69df3750db1ef34415f8f
Ihor Statsenko <statsenkoin@gmail.com>
2023-07-24 12:30:20 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
-import { Footer } from '@/components/Footer';
-import { Header } from '@/components/Header';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import { Providers } from '@/components/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        {/* <Providers> */}
         <Header />
         <main>{children}</main>
         <Footer />
+        {/* </Providers> */}
       </body>
     </html>
   );
