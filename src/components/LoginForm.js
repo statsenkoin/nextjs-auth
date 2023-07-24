b6e95dcc0873fee3733a426e8b4d6a134d42be95
Ihor Statsenko <statsenkoin@gmail.com>
2023-07-24 21:14:27 +0300

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,53 @@
 'use client';
 
 // import { signIn } from 'next-auth/react';
-// import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { toast } from 'react-hot-toast';
 
 import styles from '@/app/page.module.css';
 
 export const LoginForm = () => {
-  // const router = useRouter();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
-    // const res = await signIn('credentials', {
-    //   email: formData.get('email'),
-    //   password: formData.get('password'),
-    //   redirect: false,
-    // });
 
-    // if (res && !res.error) {
-    //   window.alert('User logged in successfully');
-    //   router.push('/');
-    // } else {
-    //   console.log('res- :>> ', res);
-    // }
+    const userData = {
+      email: formData.get('email'),
+      password: formData.get('password'),
+    };
+
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const user = await res.json();
+
+      if (user && !user.error) {
+        console.log('user+ :>> ', user);
+        toast.success('User logged in successfully');
+
+        router.push('/');
+      } else {
+        console.log('user- :>> ', user.error);
+        toast.error(user.error);
+      }
+    } catch (error) {
+      console.log('error :>> ', error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -43,7 +67,7 @@ export const LoginForm = () => {
         className={styles.input}
       />
       <button type="submit" className={styles.loginButton}>
-        Sign In
+        {loading ? 'Processing...' : 'Login'}
       </button>
     </form>
   );
