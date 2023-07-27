4ee71eb3638989faab919acf69b64d8d86262dc5
Ihor Statsenko <statsenkoin@gmail.com>
2023-07-27 23:39:23 +0300

    Update Providers.js

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -1,7 +1,13 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { Toaster } from 'react-hot-toast';
 
 export const Providers = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      {children}
+      <Toaster />
+    </SessionProvider>
+  );
 };
