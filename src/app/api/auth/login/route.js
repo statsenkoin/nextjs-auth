d8c8c8dbbc4289575312d73c6a55f302b89a9e61
Ihor Statsenko <statsenkoin@gmail.com>
2023-07-27 23:37:12 +0300

    update login route

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
 import { connectDb } from '@/configs/dbConfig';
 import User from '@/models/user';
+// import Account from '@/models/account';
 
 connectDb();
 
@@ -11,48 +12,69 @@ export async function POST(request) {
   try {
     const body = await request.json();
 
-    console.log('body Credentials :>> ', body);
+    // console.log('loginRoute body:>> ', body);
     const { email, password } = body;
 
     //check if user already exists
     const user = await User.findOne({ email });
 
-    console.log('user :>> ', user);
-
     if (!user) {
-      return NextResponse.json({ error: 'No such user in DB' }, { status: 400 });
+      // return NextResponse.json({ error: 'No such user in DB' }, { status: 400 });
+      return NextResponse.json({ error: 'Email or password is wrong' }, { status: 400 });
     }
 
     //check password
     const comparePassword = await bcrypt.compare(password, user.password);
 
     if (!comparePassword) {
-      return NextResponse.json({ error: 'Email or password is wrong' }, { status: 401 });
+      return NextResponse.json({ error: 'Email or password is wrong' }, { status: 400 });
     }
 
-    // create token
-    // const jwtPaylooad = {
-    //   id: user._doc._id,
-    //   name: user._doc.name,
-    //   email: user._doc.email,
-    //   image: user._doc.image,
-    // };
-    // const accessToken = jwt.sign(jwtPaylooad, process.env.JWT_SECRET, {
+    const userData = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      image: user.image,
+    };
+
+    console.log('loginRoute userData:>> ', userData);
+
+    return NextResponse.json(userData, { status: 200 });
+
+    // // const refresh_token = jwt.sign(userData, process.env.JWT_SECRET, {
+    // //   expiresIn: '30d',
+    // // });
+    // const access_token = jwt.sign(userData, process.env.JWT_SECRET, {
     //   expiresIn: '1h',
     // });
-    // const result = {
-    //   ...jwtPaylooad,
-    //   accessToken,
-    // };
+    // // const decodedJwt = jwt.decode(access_token);
+    // // console.log('decodedJwt :>> ', decodedJwt);
+    // // const result = {
+    // //   ...jwtPaylooad,
+    // //   access_token,
+    // // };
 
-    // const savedUser = await User.findByIdAndUpdate(user._doc._id, {
+    // const account = {
+    //   userId: user._id,
+    //   // refresh_token,
+    //   access_token,
+    //   expires_at: jwt.decode(access_token).exp,
+    //   token_type: 'Bearer',
+    // };
+    // console.log('account :>> ', account);
+
+    // const newAccount = await Account.create(account);
+    // ==============================================
+
+    // const savedUser = await User.findByIdAndUpdate(user._id, {
     //   accessToken,
     // });
     // console.log('savedUser :>> ', savedUser);
 
     // return new Response(JSON.stringify(result, { status: 200 }));
     // return NextResponse.json(savedUser, { status: 200 });
-    return NextResponse.json(user, { status: 200 });
+    // return NextResponse.json({ user: userData, account }, { status: 200 });
+    // return NextResponse.json(body, { status: 200 });
   } catch (error) {
     console.log('error :>> ', error);
     return NextResponse.json(null);
