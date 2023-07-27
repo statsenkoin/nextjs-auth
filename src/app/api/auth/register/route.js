2f16b67456d3bd9169de5d451c57c369bcaac7b8
Ihor Statsenko <statsenkoin@gmail.com>
2023-07-27 23:37:44 +0300

    update register route

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -19,20 +19,20 @@ export async function POST(request) {
     //hash password
     const hashedPassword = await bcrypt.hash(body.password, 10);
 
-    //prepare user dats and save new user
+    //prepare user data and save new user
     const newUser = { ...body, password: hashedPassword };
-
-    const savedUser = await User.create(newUser);
-
-    //return only needed fields
-    const userWithoutPass = {
-      id: savedUser._id,
-      name: savedUser.name,
-      email: savedUser.email,
-    };
+    await User.create(newUser);
 
     return NextResponse.json(body, { status: 201 });
-    // return NextResponse.json(userWithoutPass, { status: 201 });
+
+    // //return only needed fields
+    // const savedUser = await User.create(newUser);
+    // const userWithoutPass = {
+    //   id: savedUser._id,
+    //   name: savedUser.name,
+    //   email: savedUser.email,
+    // };
+    // // return NextResponse.json(userWithoutPass, { status: 201 });
   } catch (error) {
     console.log('error :>> ', error);
     return NextResponse.json({ error }, { status: 500 });
